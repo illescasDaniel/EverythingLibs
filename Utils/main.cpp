24f43e880a6d9baf22afa128c9e55e2c34a5a443
illescasDaniel <illescas.daniel@protonmail.com>
2017-07-07 17:40:11 +0200

    Initial Commit

---
+++ Utils/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Utils
+//
+//  Created by Daniel Illescas Romero on 07/07/2017.
+//  Copyright © 2017 Daniel Illescas Romero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
