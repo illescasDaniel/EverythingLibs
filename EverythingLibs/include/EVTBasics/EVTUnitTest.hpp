9e8d02cab1946b39784ddfce2f2dcd429e17000b
illescasDaniel <illescas.daniel@protonmail.com>
2017-12-05 22:21:33 +0100

    Improved unit test

--- EverythingLibs/include/EVTBasics/EVTUnitTest.hpp
+++ EverythingLibs/include/EVTBasics/EVTUnitTest.hpp
@@ -26,6 +26,8 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <functional>
 
 namespace evt {
 	
@@ -49,6 +51,7 @@ namespace evt {
 				} else {
 					std::cout << "✗ TEST FAILED (\"" << this->testName << "\"): \"" << errorMessage << "\"" << std::endl;
 				}
+				
 				exit(1);
 			}
 		}
@@ -64,6 +67,13 @@ namespace evt {
 			else {
 				std::cout << "\n✓ ALL TESTS PASSED [\"" << testName << "\"]\n" << std::endl;
 			}
+		}
+		
+		static void run(const std::string& testName, std::vector<std::function<void()>> tests) {
+			for (const auto& test: tests) {
+				test();
+			}
+			evt::UnitTest::testPassed(testName);
 		}
 		
 		//
